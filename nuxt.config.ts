c3edd6ecbffd6f91a1e8f86a1eed5334ecc9583f
Gianluca Zicca <gianluca.zicca@bestit.de>
2024-08-29 23:43:49 +0200

    added shadcn to the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
     srcDir: 'src/',
-    modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
+    modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
     eslint: {
         config: {
             standalone: false,
@@ -14,4 +14,15 @@ export default defineNuxtConfig({
         configPath: '~~/config/tailwind.config',
 
     },
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: '',
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: '~~src/components/ui'
+    }
 })
